e218b9e2b5bc638f22afe96fcf93056890344b1b
patriciaderieux <79328776+patriciaderieux@users.noreply.github.com>
2022-04-27 15:35:33 -0500

    setting up routes

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import authRoutes from './routes/auth';
+
+
+const app = express();
+
+app.use(express.json());
+app.use('/auth', authRoutes);
+
+app.listen(8080, () => {
+    console.log(`Now listening to port 8080`)
+})
