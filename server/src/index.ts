bef6919708682646836857a86e48162dc9b09a4c
patriciaderieux <79328776+patriciaderieux@users.noreply.github.com>
2022-04-28 16:56:37 -0500

    added the connection to db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,37 @@
 import express from 'express';
 import authRoutes from './routes/auth';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 const app = express();
+
+dotenv.config();
 
 app.use(express.json());
 app.use('/auth', authRoutes);
 
-app.listen(8080, () => {
-    console.log(`Now listening to port 8080`)
+
+mongoose.connect(
+    process.env.MONGO_URI as string
+)
+.then(() => {
+    console.log("Connected! to mongoDB");
+
+    app.listen(8080, () => {
+        const app = express();
+
+
+
+app.use(express.json());
+app.use('/auth', authRoutes);
+
+        console.log(`Now listening to port 8080`)
+    })
 })
+.catch((error) => {
+    console.log({error})
+    throw new Error(error)
+})
+
