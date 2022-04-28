c81be83cc0eefe0a8bda40c24e5286cb88f648ba
patriciaderieux <79328776+patriciaderieux@users.noreply.github.com>
2022-04-28 10:23:35 -0500

    adding the auth

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,10 +1,23 @@
 import express from "express";
-
+import { body, validationResult } from 'express-validator';
 
 
 const router = express.Router();
 
-router.post('/signup', async (req, res) => {
+router.post('/signup', body('email').isEmail().withMessage('The emai is invalid'), body('password').isLength({min: 5}).withMessage('THe password is invalid'), async (req, res) => {
+
+    const validationErrors = validationResult(req);
+
+    if(!validationErrors.isEmpty()) {
+        const errors = validationErrors.array().map(error => {
+            return {
+                msg: error.msg
+            }
+        })
+
+        return res.json({ errors });
+    }
+
     const { email, password } = req.body;
 
     res.json({
