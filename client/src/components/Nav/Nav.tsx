e2d65268feaab4f0ed24a820cc81724d99a91a2e
patriciaderieux <79328776+patriciaderieux@users.noreply.github.com>
2022-05-02 15:58:16 -0500

    handle logout in the articles page

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -1,7 +1,23 @@
 import { Navbar, NavItem, NavLink } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../../context";
+import styled from "styled-components";
+
+const LeftNavContainer = styled.div`
+  margin-left: auto;
+`;
 
 const Nav = () => {
+  const [state, setState] = useContext(UserContext);
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    setState({ data: null, loading: false, error: null });
+    localStorage.removeItem("token");
+    navigate("/");
+  };
   return (
     <Navbar>
       <NavItem>
@@ -9,6 +25,13 @@ const Nav = () => {
           Home
         </Link>
       </NavItem>
+      {state.data && (
+        <LeftNavContainer>
+          <NavItem>
+            <NavLink onClick={handleLogout}>Logout</NavLink>
+          </NavItem>
+        </LeftNavContainer>
+      )}
     </Navbar>
   );
 };
