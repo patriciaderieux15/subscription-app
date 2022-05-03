ac401a4b8c264bd541ef199a28fee894b5b4536b
patriciaderieux <79328776+patriciaderieux@users.noreply.github.com>
2022-05-03 15:09:49 -0500

    adding the price plan

--- client/src/pages/ArticlesPlan.tsx
+++ client/src/pages/ArticlesPlan.tsx
@@ -19,6 +19,23 @@ const CardHeader = styled.div`
   justify-content: center;
 `;
 
+const PriceCircle = styled.div`
+  border: 0.5rem solid white;
+  width: 12.5rem;
+  height: 12.5rem;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0.1rem 0.1rem 1rem rgba(19, 20, 19, 0.342);
+`;
+
+const PriceText = styled.p`
+  font-size: 3rem;
+  color: white;
+  text-shadow: 0.1rem 0.1rem 1rem rgba(19, 20, 19, 0.342);
+`;
+
 const ArticlesPlan = () => {
   const [prices, setPrices] = useState<any[]>([]);
 
@@ -40,7 +57,11 @@ const ArticlesPlan = () => {
             <Card
               style={{ width: "18rem", height: "25rem", marginRight: "2rem" }}
             >
-              <CardHeader>Test test</CardHeader>
+              <CardHeader>
+                <PriceCircle>
+                  <PriceText>${price.unit_amount / 100}</PriceText>
+                </PriceCircle>
+              </CardHeader>
             </Card>
           );
         })}
